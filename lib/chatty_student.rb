816421c569ce7be8e5967274195c1b784902792a
Alehandro Persaud <andrew041994@gmail.com>
2020-02-08 21:25:17 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,9 +1,9 @@
 require_relative "./student.rb"
 class ChattyStudent
   def hello
-  greeting = "Hey there! I'm so excited to learn stuff.\n"
+    super
   long_chat = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
-  puts "#{greeting}#{long_chat}"
+  puts long_chat
 
   end
 
