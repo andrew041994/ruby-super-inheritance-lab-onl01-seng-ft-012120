5ce3a5fb6afb2a5cd7f9c371c966c7c90be61649
Alehandro Persaud <andrew041994@gmail.com>
2020-02-08 21:26:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,11 @@ class Student
   def hello
     greeting = "Hey there! I'm so excited to learn stuff.\n"
     puts greeting
+  end
+
+  def raise_hand
+    phrase = "Pick me!"
+    puts phrase
 
   end
 
