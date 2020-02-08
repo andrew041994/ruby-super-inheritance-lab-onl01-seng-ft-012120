910d9c9c10d7d97ba3f222be7d18c52d80b57935
Alehandro Persaud <andrew041994@gmail.com>
2020-02-08 21:29:21 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,12 @@
+class Student
+
+  def hello
+    greeting = "Hey there! I'm so excited to learn stuff.\n"
+    puts greeting
+  end
+
+  def raise_hand
+    phrase = "Pick me!"
+    puts phrase
+  end
+end
